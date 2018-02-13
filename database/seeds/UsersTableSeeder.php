90d2ba0129b26983da248f12150e1d613a60f27e
jvieyra <vieyrajpl@hotmail.com>
2018-02-13 17:55:10 -0600

    Creacion de Seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+//import model User
+use App\User;
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+          'name'=>'Jake',
+          'email'=>'kaje@hotmail.com',
+          'password'=> bcrypt('123456')
+        ]);
+    }
+}
